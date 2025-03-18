bc90ef956d4b5109fb5cb40cdc0ec66f78e09f63
MT KANANI <mtkanani8776@gmail.com>
2025-03-18 09:04:04 +0530

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,16 @@
+var k={
+    name:'Test', //str
+    age:20, //num
+    subject:{'fsd-2':'NAS','fcsp':'VHA'}, //obj
+    marks:[25,22], //array
+    present:true, //boolean
+    exam:'null' //str
+}
+console.log(k)
+console.log(k.name)
+console.log(k['name'])
+console.log(k.age)
+console.log(k.subject.fcsp)
+console.log(k.marks[1])
+console.log(k.present)
+console.log(k.exam)
